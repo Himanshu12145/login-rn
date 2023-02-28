2da3b875ccfa762cf4977bcd8c18e34d035d06c2
Himanshu12145 <himansu12145@gmail.com>
2023-02-28 20:11:16 +0530

    login signup and firebase connection done

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import { Alert } from "react-native";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../util/auth";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+  async function signupHandler({ email, password }) {
+    setIsAuthenticate(true);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not sign you up. Please check your details or try again later!"
+      );
+    }
+    setIsAuthenticate(false);
+  }
+  if (isAuthenticate) {
+    return <LoadingOverlay message="Creating User..." />;
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
